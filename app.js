b74c3290afc3dccf52b884749e466eda42be7628
Aman Aswal <amanpotato11@gmail.com>
2020-07-17 13:35:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 
 const app = express();
@@ -32,21 +34,55 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret:Array
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
  
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:4000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", (req,res)=>{
     res.render("home");
 });
+
+app.get("/auth/google",
+    passport.authenticate("google", {scope: ["profile"]})
+);
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
 
 app.get("/login", (req,res)=>{
     res.render("login");
@@ -56,14 +92,84 @@ app.get("/register", (req,res)=>{
     res.render("register");
 });
 
-app.get("/secrets", (req,res)=>{
-    if(req.isAuthenticated()){
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
-    }
+app.get("/secrets",function(req,res){
+    User.find({secret:{$ne:null}},function (err, users){
+      if(!err){
+        if (users){
+          res.render("secrets",{usersWithSecrets:users});
+        }else {
+          console.log(err);
+        }
+      }else {
+        console.log(err);
+      }
+    });
 });
 
+app.route("/submit")
+.get(function (req,res){
+  if(req.isAuthenticated()){
+    User.findById(req.user.id,function (err,foundUser){
+      if(!err){
+        res.render("submit",{secrets:foundUser.secret});
+      }
+    })
+  }else {
+    res.redirect("/login");
+  }
+})
+.post(function (req, res){
+  if(req.isAuthenticated()){
+    User.findById(req.user.id,function (err, user){
+      user.secret.push(req.body.secret);
+      user.save(function (){
+        res.redirect("/secrets");
+      });
+    });
+ 
+  }else {
+   res.redirect("/login");
+  }
+});
+
+app.post("/submit/delete",function (req, res){
+    if(req.isAuthenticated()){
+      User.findById(req.user.id, function (err,foundUser){
+        foundUser.secret.splice(foundUser.secret.indexOf(req.body.secret),1);
+        foundUser.save(function (err) {
+          if(!err){
+            res.redirect("/submit");
+          }
+        });
+      });
+    }else {
+      res.redirect("/login");
+    }
+  });
+
+// app.get("/submit", (req, res)=>{
+//     if(req.isAuthenticated()){
+//         res.render("submit");
+//     } else {
+//         res.redirect("/login");
+//     }
+// });
+
+// app.post("/submit",(function (req, res){
+//     if(req.isAuthenticated()){
+//       User.findById(req.user.id,function (err, user){
+//         user.secret.push(req.body.secret);
+//         user.save(function (){
+//           res.redirect("/secrets");
+//         });
+//       });
+   
+//     }else {
+//      res.redirect("/login");
+//     }
+// }));
+
+// Log out
 app.get("/logout", (req,res)=>{
     req.logout();
     res.redirect("/");
